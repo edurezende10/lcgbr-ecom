d02469120d2f937ec20b18952ae92982a9c7efb3
unknown <edu_rezende11@hotmail.com>
2023-06-13 17:54:43 -0300

    fix senddata

--- frontend/public/sendData.js
+++ frontend/public/sendData.js
@@ -1,14 +1,17 @@
 function sendData(data, endpoint) {
-    var xhr = new XMLHttpRequest();
-    var stringifiedData = JSON.stringify(data);
-  
-    xhr.open('POST', endpoint);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.send(stringifiedData);
-  
-    xhr.onload = function () {
-      if (xhr.status.toString()[0] !== '2') {
-        console.error(xhr.status + '> ' + xhr.statusText);
-      }
-    };
-  }
+  fetch(endpoint, {
+  method: 'POST',
+  mode: 'no-cors',
+  headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    // A resposta será opaca (opaque) devido ao modo 'no-cors'
+    // Você pode verificar response.status para ver se a solicitação foi bem-sucedida
+  })
+  .catch(error => {
+    // Trate erros aqui
+  });
+}
