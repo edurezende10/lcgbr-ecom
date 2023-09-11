16ee9f09b346905748c076aae738db79ad0ae34d
unknown <edu_rezende11@hotmail.com>
2023-09-11 13:19:48 -0300

    atualizando node

--- frontend/public/sendData.js
+++ frontend/public/sendData.js
@@ -15,3 +15,33 @@ function sendData(data, endpoint) {
     // Trate erros aqui
   });
 }
+/* 
+.html 
+/carrinho.html
+
+content/b2b/e-commerce-equipamentos/vitrine.html
+content/b2b/e-commerce-equipamentos/vitrine/carrinho.html
+content/b2b/e-commerce-equipamentos/vitrine/produtos/categoria.html?title=Inform%C3%A1tica&query=:categorySectionDisplay:allCategories:Inform%C3%A1tica.html
+
+
+
+/content/b2b/e-commerce-equipamentos/vitrine.html 
+/content/b2b/e-commerce-equipamentos/vitrine/carrinho.html 
+/content/b2b/e-commerce-equipamentos/vitrine/produtos/categoria.html
+/content/b2b/e-commerce-equipamentos/vitrine/produtos/categoria.html
+/content/b2b/e-commerce-equipamentos/vitrine/produtos/categoria.html
+/content/b2b/e-commerce-equipamentos/vitrine/produtos/categoria.html
+/content/b2b/e-commerce-equipamentos/vitrine/ajuda.html 
+
+
+/content/b2b/e-commerce-equipamentos/vitrine/produto/produto.html
+
+
+samuel.rsilva@grupopan.com
+
+ */
+
+
+
+
+TAG: View:Resumo:Oferta - ["bp.data.banner.hposition": "3", "bp.data.banner.name": "vei_home-res-1_seg_shopping-bau-img", "bp.data.pagename": "BP:APP:Home:Resumo", "bp.data.banner.vposition": "Home", "bp.data.subsecao1": "Home", "bp.data.estadologin": "LG", "bp.data.secao": "ContaDigital"]
